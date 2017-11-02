83ce3d3226bc3f7f7a44b1d9fea293f65a6daeeb
Stavros Skouras <skouras.stavros@gmail.com>
2017-11-02 11:12:03 +0100

    edited rtQC_xtc2nii

--- api-matlab/rtQC_xtc2nii.m
+++ api-matlab/rtQC_xtc2nii.m
@@ -1,29 +1,59 @@
+function rtQC_xtc2nii(xtcDir, niiOutDir, template_hdr_file, volume, nSlices)  
 
+%{ 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 Function name: rtQC_xtc2nii
 
-Description: This function converts data acquired from the Philips XTC real-time export module (PAR/REC files) to Nifti format.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Description: This function converts data acquired using the Philips XTC
+ real-time export module (PAR/REC files) to NIfTI format.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 Usage: rtNII_volume = rtQC_xtc2nii(xtc_ParRec_volume, template_volume)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 Arguments, inputs and outputs:
 
 xtc_ParRec_volume ~ Realtime volume dumped by XTC in Par/Rec format
 
-template_volume ~ a volume acquired using the same scanning sequence that is used as reference to complete missing header attributes
+template_volume ~ a volume acquired using the same scanning sequence that 
+is used as reference to complete missing header attributes
 
-rtNII_volume ~ typical nii file you can readily use in SPM or other software, corresponding exactly to what you would have gotten if you had exported the data offline into dicom and then converted it to nii using dcm2nii from MRIcro.
+rtNII_volume ~ typical nii file you can readily use in SPM or other 
+software, corresponding exactly to what you would have gotten if you had 
+exported the data offline into dicom and then converted it to nii using 
+dcm2nii from MRIcro.
 
-Special notes: not applicable
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Dependencies: dcm2nii from MRIcro
+Special notes: 
+To add soon: storing timing data and printing at the end of the sequence
 
-Contributors: Stavros
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Dependencies: dcm2nii from MRIcro, loadPARREC.m and SPM.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% AUTHOR: Stavros Skouras; Barcelonabeta Brain Research Center; 17/02/2017.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% LICENSE: GPLv3 / please cite author
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% WARNING: Use at your own risk!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%}
 
 
+    [DATA INFO]=loadPARREC([xtcDir '/Dump-' sprintf('%04d',(volume-1)) '.par']);
+    for slice=1:nSlices
+        PAR(:,:,slice) = fliplr(DATA(:,:,slice,1,1,1)');
+    end
+    infoVol = spm_vol(template_hdr_file);
+    infoVol.fname=[niiOutDir '\Volume' sprintf('%03d',volume) '.nii'];
+    infoVol=rmfield(infoVol,'pinfo'); spm_write_vol(infoVol,PAR);
+    
 
-%% ability to compare stuff --
-
-% some history tracking in rtqc structure
-% 
-% 
+return
